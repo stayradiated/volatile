08f3225b227fb333bb40ae0b2350feddd51eb697
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-06-27 22:02:09 +1200

    feat: replace keyring with pgp

--- kc-keyring/src/index.ts
+++ kc-keyring/src/index.ts
@@ -1,19 +1,80 @@
-import fs from 'fs/promises'
+import * as openpgp from 'openpgp'
 
-import { UserKeys, Algorithm, createKeyring } from './keyring.js'
-
-type FileContents = {
-  algorithm: Algorithm
-  keys: UserKeys
+type GenerateKeyPairOptions = {
+  userId: openpgp.UserID
+  passphrase: string
 }
 
-const createKeyringFromFilepath = async (filepath: string) => {
-  const fileContents = JSON.parse(
-    await fs.readFile(filepath, 'utf8'),
-  ) as FileContents
-  const { algorithm, keys } = fileContents
-  const keyring = createKeyring(keys, { algorithm })
-  return keyring
+type KeyPair = {
+  privateKey: string
+  publicKey: string
 }
 
-export { createKeyring, createKeyringFromFilepath }
+const generateKeyPair = async (
+  options: GenerateKeyPairOptions,
+): Promise<KeyPair> => {
+  const { userId, passphrase } = options
+
+  const keypair = await openpgp.generateKey({
+    type: 'ecc',
+    curve: 'curve25519',
+    userIDs: [userId],
+    passphrase,
+    keyExpirationTime: 0, // Never expires
+  })
+
+  return {
+    privateKey: keypair.privateKeyArmored,
+    publicKey: keypair.publicKeyArmored,
+  }
+}
+
+type EncryptOptions = {
+  publicKey: string
+  message: string
+}
+
+const encrypt = async (options: EncryptOptions): Promise<string> => {
+  const { publicKey: publicKeyArmored, message: messageText } = options
+
+  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored })
+  const message = await openpgp.createMessage({ text: messageText })
+
+  const encrypted = await openpgp.encrypt({
+    message,
+    encryptionKeys: publicKey,
+    armor: true,
+  })
+
+  return encrypted
+}
+
+type DecryptOptions = {
+  passphrase: string
+  privateKey: string
+  encryptedMessage: string
+}
+
+const decrypt = async (options: DecryptOptions): Promise<string> => {
+  const {
+    passphrase,
+    privateKey: privateKeyArmored,
+    encryptedMessage,
+  } = options
+
+  const privateKey = await openpgp.decryptKey({
+    passphrase,
+    privateKey: await openpgp.readPrivateKey({
+      armoredKey: privateKeyArmored,
+    }),
+  })
+
+  const decrypted = await openpgp.decrypt({
+    message: await openpgp.readMessage({ armoredMessage: encryptedMessage }),
+    decryptionKeys: privateKey,
+  })
+
+  return decrypted.data
+}
+
+export { generateKeyPair, encrypt, decrypt }
