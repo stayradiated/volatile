6d969656acb355d9d7eb4cb7ab65db578a6095d7
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-07-05 18:59:13 +1200

    feat(market-price): +5m cache time to openexchangerates

--- market-price/src/currencies/open-exchange-rates.ts
+++ market-price/src/currencies/open-exchange-rates.ts
@@ -41,7 +41,7 @@ const createMarketSourceForCurrency = (
 
   const marketSource: MarketPriceSource<Options> = {
     log,
-    minCacheDuration: Duration.fromISOTime('01:00:00'),
+    minCacheDuration: Duration.fromISOTime('01:05:00'),
     fetch: async (options) => {
       const { config } = options
       const { appId } = config
