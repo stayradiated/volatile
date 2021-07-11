4cebc4a93a77b07afd25fc10a655089d0a1a6a3a
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-07-11 20:34:29 +1200

    feat(kc-server): add fastify

--- kc-server/src/index.ts
+++ kc-server/src/index.ts
@@ -1,11 +1,22 @@
 import { readConfig } from '@stayradiated/kc-config'
+import createFastify from 'fastify'
 
-import { CONFIG_PATH } from './env.js'
+import { CONFIG_PATH, PORT } from './env.js'
 import pool from './pg-pool.js'
 import { fetchMarketPrice } from './components/market-price/index.js'
-import { initAutoBuy } from './components/auto-buy/index.js'
+// Import { initAutoBuy } from './components/auto-buy/index.js'
 
 import type { ComponentProps } from './types.js'
+
+const fastify = createFastify({
+  logger: true,
+})
+
+fastify.post('/users', async (request, reply) => {
+  await reply.send(request.body)
+})
+
+fastify.listen(PORT)
 
 void (async function () {
   const config = await readConfig(CONFIG_PATH)
@@ -20,5 +31,8 @@ void (async function () {
     pool,
   }
 
-  await Promise.all([fetchMarketPrice(props), initAutoBuy(props)])
+  await Promise.all([
+    fetchMarketPrice(props),
+    // , initAutoBuy(props)
+  ])
 })()
