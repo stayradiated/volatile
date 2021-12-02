73fe1b78c3b1d19b205b4aebb3436c6ed51e18d5
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-12-02 07:47:22 +0100

    fix(kc-web): build all pages (not just index.html)

--- kc-web/vite.config.ts
+++ kc-web/vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        'dca-orders': resolve(__dirname, 'dca-orders/index.html'),
+        'devices': resolve(__dirname, 'devices/index.html'),
+        'login': resolve(__dirname, 'login/index.html'),
+        'main': resolve(__dirname, 'index.html'),
+        'open-orders': resolve(__dirname, 'open-orders/index.html'),
+        'register': resolve(__dirname, 'register/index.html'),
+        'settings': resolve(__dirname, 'settings/index.html'),
+        'trades': resolve(__dirname, 'trades/index.html'),
+      }
+    }
+  }
 })
