e8059d8dfc3c457c6e7ea1a5d68a36bba2215367
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-01-19 07:23:04 +0100

    feat(server): increase default limit to 5 DCA orders

--- svc-server/src/model/user-limit/get-user-limit.ts
+++ svc-server/src/model/user-limit/get-user-limit.ts
@@ -3,7 +3,7 @@ import type { Pool } from '../../types.js'
 
 const getUserLimit = async (_pool: Pool, _userUID: string): Promise<UserLimit|Error> => {
   return {
-    maxEnabledDCAOrderCount: 2
+    maxEnabledDCAOrderCount: 5
   }
 }
 
