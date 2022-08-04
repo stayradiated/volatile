5a8e35733175496768b6688071aceb84a46faed1
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-08-04 21:34:52 +0100

    deprecate svc-web, svc-admin

--- svc-server/src/util/stripe.ts
+++ svc-server/src/util/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import { config } from '../env.js'
 
 const stripe = new Stripe(config.STRIPE_API_KEY, {
-  apiVersion: '2020-08-27',
+  apiVersion: '2022-08-01',
 })
 
 export { stripe }
